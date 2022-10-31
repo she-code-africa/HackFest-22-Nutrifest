07e600405f92c5b7b4cb542bb98d4533a0c6762f
Tanitoluwa <tanitoluwaadenuga@gmail.com>
2022-10-31 01:09:40 +0100

    added audio content till the next slep

--- audio.js
+++ audio.js
@@ -1,59 +1,47 @@
 Amplitude.init({
    "songs":[
       {
-         "name":"Stress Management - Building Awareness",
-         "url":"./audio/Building_Awareness.mp3",
-         "source": "World Health Organization",
-         "source_url": "https://www.who.int/publications/i/item/9789240003927"
-      },
-      {
-         "name":"Stress Management - Grounding Exercise 1",
-         "url":"./audio/Grounding_Exercise_1.mp3",
-         "source": "World Health Organization",
-         "source_url": "https://www.who.int/publications/i/item/9789240003927"
-      },
-      {
-         "name":"Stress Management - Grounding Exercise 2",
-         "url":"./audio/Grounding_Exercise_2.mp3",
-         "source": "World Health Organization",
-         "source_url": "https://www.who.int/publications/i/item/9789240003927"
-      },
-      {
-         "name":"Stress Management - Grounding Exercise 3",
-         "url":"./audio/Grounding_Exercise_3.mp3",
-         "source": "World Health Organization",
-         "source_url": "https://www.who.int/publications/i/item/9789240003927"
-      },
-      {
-         "name":"Stress Management - Notice and Name",
-         "url":"./audio/Notice_and_Name.mp3",
-         "source": "World Health Organization",
-         "source_url": "https://www.who.int/publications/i/item/9789240003927"
-      },
-      {
-         "name":"Stress Management - Unhooking from Unkind Thoughts",
-         "url":"./audio/Unhooking_from_Unkind_Thoughts.mp3",
-         "source": "World Health Organization",
-         "source_url": "https://www.who.int/publications/i/item/9789240003927"
-      },
-      {
-         "name":"Stress Management - Making Room",
-         "url":"./audio/Making_Room.mp3",
-         "source": "World Health Organization",
-         "source_url": "https://www.who.int/publications/i/item/9789240003927"
-      },
-      {
-         "name":"Stress Management - Being Kind to Yourself",
-         "url":"./audio/Being_kind_to_yourself.mp3",
-         "source": "World Health Organization",
-         "source_url": "https://www.who.int/publications/i/item/9789240003927"
-      },
-      {
-         "name":"malnutrition and hunger - lfbi",
-         "url":"./audio/LFBI-1-woman_edited.mp3",
+         "name":"Malnutrition Reduction - Introduction",
+         "url":"./audio/intro-adenike.mp3",
          "source": "Lagos Food Bank Initiative",
-         "source_url": "https://lagosfoodbank.org/about-us/who-we-are/"
+         "source_url": "https://lagosfoodbank.org/about-us/"
       },
-  
+      {
+         "name":"Malnutrition Reduction - Symptomps and Effects",
+         "url":"./audio/emily-content.mp3",
+         "source": "Lagos Food Bank Initiative",
+         "source_url": "https://www.who.int/publications/i/item/9789240003927"
+      },
+      {
+         "name":"Malnutrition Reduction - Benefits of a healthy diet & tips to curb nutrition",
+         "url":"./audio/emily-4.mp3",
+         "source": "Lagos Food Bank Initiative",
+         "source_url": "https://www.who.int/publications/i/item/9789240003927"
+      },
+      {
+         "name":"Malnutrition Reduction - A Food Bank?",
+         "url":"./audio/LFBI-4-Funmi-edited.mp3",
+         "source": "Lagos Food Bank Initiative",
+         "source_url": "https://www.who.int/publications/i/item/9789240003927"
+      },
+      {
+         "name":"Malnutrition Reduction - LFBI's Approach to curbing Malnutrition",
+         "url":"./audio/LFBI-3-Funmi-edited.mp3",
+         "source": "Lagos Food Bank Initiative",
+         "source_url": "https://www.who.int/publications/i/item/9789240003927"
+      },
+      {
+         "name":"Malnutrition Reduction - LFBI Testimonial",
+         "url":"./audio/LFBI-1-womam_edited.mp3",
+         "source": "Lagos Food Bank Initiative",
+         "source_url": "https://www.who.int/publications/i/item/9789240003927"
+      },
+      {
+         "name":"Malnutrition Reduction - LFBI Testimonial",
+         "url":"./audio/LFBI-2-woman.mp3",
+         "source": "Lagos Food Bank Initiative",
+         "source_url": "https://www.who.int/publications/i/item/9789240003927"
+      },
+      
    ]
 });
