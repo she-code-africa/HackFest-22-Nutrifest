17b3090d123772144cb57d9abade4d21e4aefdfa
Tanitoluwa <tanitoluwaadenuga@gmail.com>
2022-10-31 08:35:43 +0100

    sdded a new audio

--- audio.js
+++ audio.js
@@ -7,37 +7,31 @@ Amplitude.init({
          "source_url": "https://lagosfoodbank.org/about-us/"
       },
       {
-         "name":"Malnutrition Reduction - Symptomps and Effects",
+         "name":"Malnutrition Reduction - Symptoms and Effects",
          "url":"./audio/emily-content.mp3",
          "source": "Lagos Food Bank Initiative",
          "source_url": "https://www.who.int/publications/i/item/9789240003927"
       },
       {
-         "name":"Malnutrition Reduction - Benefits of a healthy diet & tips to curb nutrition",
+         "name":"Malnutrition Reduction - Tips to curb nutrition",
          "url":"./audio/emily-4.mp3",
          "source": "Lagos Food Bank Initiative",
          "source_url": "https://www.who.int/publications/i/item/9789240003927"
       },
       {
-         "name":"Malnutrition Reduction - LFBI Introduction",
-         "url":"./audio/LFBI-4-Funmi-edited.mp3",
+         "name":"Malnutrition Reduction - LFBI's Approach",
+         "url":"./audio/funmi-merged.mp3",
          "source": "Lagos Food Bank Initiative",
          "source_url": "https://www.who.int/publications/i/item/9789240003927"
       },
       {
-         "name":"Malnutrition Reduction - LFBI's Approach to curbing Malnutrition",
-         "url":"./audio/LFBI-3-Funmi-edited.mp3",
-         "source": "Lagos Food Bank Initiative",
-         "source_url": "https://www.who.int/publications/i/item/9789240003927"
-      },
-      {
-         "name":"Malnutrition Reduction - LFBI Testimonial-1",
+         "name":"Malnutrition Reduction - LFBI Testimonial 1",
          "url":"./audio/LFBI-1-womam_edited.mp3",
          "source": "Lagos Food Bank Initiative",
          "source_url": "https://www.who.int/publications/i/item/9789240003927"
       },
       {
-         "name":"Malnutrition Reduction - Malnutrition Reduction - LFBI Testimonials 2",
+         "name":"Malnutrition Reduction- LFBI Testimonial 2",
          "url":"./audio/LFBI-2-woman_edited.mp3",
          "source": "Lagos Food Bank Initiative",
          "source_url": "https://www.who.int/publications/i/item/9789240003927"
